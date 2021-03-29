e72b9ae196e44ea2de8bd8dc1ee20b97df14a84e
Maxim <konstantinovmax@gmail.com>
2021-03-29 18:44:32 +0300

    feat: применить базовые свойства

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 const canvasBody = document.querySelector('.canvas');
 const canvas = canvasBody.getContext('2d');
-const width = canvasBody.width = window.innerWidth;
-const height = canvasBody.height = window.innerHeight;
+const canvasWidth = canvasBody.width = window.innerWidth;
+const canvasHeight = canvasBody.height = window.innerHeight;
 
-canvas.fillStyle = 'magenta';
-canvas.arc(canvasBody.width / 2, canvasBody.height / 2, 100, 0, Math.PI * 2);
+canvas.fillStyle = '#000';
+canvas.arc(canvasWidth / 2, canvasHeight / 2, 50, 0, Math.PI * 2);
 canvas.fill();
 
 

e1a4738d512e04f63ec8e4dff0c8ed74ecb8af42
Maxim <konstantinovmax@gmail.com>
2021-03-29 20:12:16 +0300

    feat: реализовать функционал рисования

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,41 @@
 const canvasBody = document.querySelector('.canvas');
+const clearButton = document.querySelector('.clear-button');
 const canvas = canvasBody.getContext('2d');
 const canvasWidth = canvasBody.width = window.innerWidth;
 const canvasHeight = canvasBody.height = window.innerHeight;
+let isMouseDown = false;
 
-canvas.fillStyle = '#000';
-canvas.arc(canvasWidth / 2, canvasHeight / 2, 50, 0, Math.PI * 2);
-canvas.fill();
+canvasBody.addEventListener('mousedown', function() {
+    isMouseDown = true;
+});
 
+canvasBody.addEventListener('mouseup', function() {
+    isMouseDown = false;
+    canvas.beginPath();
+});
 
+canvas.lineWidth = 3 * 2;
+
+canvasBody.addEventListener('mousemove', function(e) {
+    if (isMouseDown) {
+        canvas.lineTo(e.clientX, e.clientY);
+        canvas.stroke();
+
+        canvas.beginPath();
+        canvas.arc(e.clientX, e.clientY, 3, 0, Math.PI * 2);
+        canvas.fill();
+
+        canvas.beginPath();
+        canvas.moveTo(e.clientX, e.clientY);
+    }
+});
+
+function clear() {
+    canvas.fillStyle = 'white';
+    canvas.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    canvas.beginPath();
+    canvas.fillStyle = 'black';
+}
+
+clearButton.addEventListener('click', clear);
